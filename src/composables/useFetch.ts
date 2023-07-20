185ed7375a10764070b2ce9cfcc74cc4d830defb
birutalukas <biruta.lukas@gmail.com>
2023-07-20 17:11:39 +0300

    remove console log

--- src/composables/useFetch.ts
+++ src/composables/useFetch.ts
@@ -1,9 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
 const getRequest = async <T>(url: string): Promise<AxiosResponse<T>> => {
-    console.log("getRequest()");
     const response = await axios.get<T>(url);
-    console.log(response.data);
     return response;
 };
 
